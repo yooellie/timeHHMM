3ded085359dbd6adc2cbf108fdc82c5c334397ea
yooellie <yooellie@outlook.com>
2016-12-30 10:28:17 +0800

    Update my-timeHHMM.js
    many time input

--- my-timeHHMM.js
+++ my-timeHHMM.js
@@ -1,9 +1,7 @@
-﻿"use strict";
-
 /*
  * timeHHMM v0.1
  * Date: 2016-12-21
- * timeHHMM为时间控件，时间格式为00:00，在需要时间的input框中添加class=".timeHHmm",在scirpt中添加$(".timeHHmm").timeHHMM();即可，本插件依赖jquery
+ * timeHHMM为时间控件，时间格式为00:00，在需要时间的input框中添加class=".xxx",在scirpt中添加$(".xxx").timeHHMM();即可，本插件依赖jquery
  */
 (function ($) {//生成modal
 
@@ -45,59 +43,57 @@
     modalObj.find(".hours").html(hourHtml);
     modalObj.find(".min").html(minHtml);
 
-
     $.fn.timeHHMM = function () {
-
-        var INPUT = this;
-        var MODEL = null;
-        var ifShow = false;
-
-        //插件实现代码
-        $(":ROOT").click(function (event) {
-            if (MODEL !== null) {
-                if (ifShow && !MODEL.find(event.target).length) {
-                    hide();
-                }
-            }
-        });
-        INPUT.on('click', function (event) {
-            event.stopPropagation();
-            if (MODEL === null) {
-                INPUT.after(modalObj.html());
-                MODEL = INPUT.next('.modalTime');
-                bindModelEvent();
-            }
-            show();
-        });
-
-
-        function bindModelEvent() {
-
-            //点击确定时
-            MODEL.on('click', ".sureTime", function () {
-                var chooseHour = MODEL.find(".hours").val();
-                var chooseMin = MODEL.find(".min").val();
-                var chooseTime = chooseHour + ":" + chooseMin;
-                INPUT.val(chooseTime);
-                hide();
-            });
-
-            //点击取消时间
-            MODEL.on('click', ".cancelTime", function () {
-                hide();
-            });
-
-        }
-
-        function show() {
-            MODEL.show();
-            ifShow = true;
-        }
-
-        function hide() {
-            MODEL.hide();
-            ifShow = false;
-        }
+	this.each(function(){
+	        var INPUT = $(this);
+	        var MODEL = null;
+	        var ifShow = false;
+	
+	        //插件实现代码
+	        $(":ROOT").click(function (event) {
+	            if (MODEL !== null) {
+	                if (ifShow && !MODEL.find(event.target).length && !INPUT.is(event.target)) {
+	                    hide();
+	                }
+	            }
+	        });
+	        INPUT.on('click', function (event) {
+	            if (MODEL === null) {
+	                INPUT.after(modalObj.html());
+	                MODEL = INPUT.next('.modalTime');
+	                bindModelEvent();
+	            }
+	            show();
+	        });
+	
+	
+	        function bindModelEvent() {
+	
+	            //点击确定时
+	            MODEL.on('click', ".sureTime", function () {
+	                var chooseHour = MODEL.find(".hours").val();
+	                var chooseMin = MODEL.find(".min").val();
+	                var chooseTime = chooseHour + ":" + chooseMin;
+	                INPUT.val(chooseTime);
+	                hide();
+	            });
+	
+	            //点击取消时间
+	            MODEL.on('click', ".cancelTime", function () {
+	                hide();
+	            });
+	
+	        }
+	
+	        function show() {
+	            MODEL.show();
+	            ifShow = true;
+	        }
+	
+	        function hide() {
+	            MODEL.hide();
+	            ifShow = false;
+	        }
+	    });
     };
 })(jQuery);
-		
